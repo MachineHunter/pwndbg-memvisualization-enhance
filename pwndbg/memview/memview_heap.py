b8b264f4391df221e349c0645fa3e82c58fa64c7
tigga36 <onotaiga36@gmail.com>
2021-09-27 09:25:39 +0000

    Fetch start of main arena

--- pwndbg/memview/memview_heap.py
+++ pwndbg/memview/memview_heap.py
@@ -4,6 +4,8 @@
 fetch information on heap
 """
 
+import pwndbg.heap
+
 class HeapInfo:
     """
     Like MemInfo, but about heaps
@@ -20,11 +22,18 @@ def get(heap_info):
         - heap_info (list): Start/end address of heap given by MemInfo class. If heap is uninitialized, value is [-1, -1]
     """
 
+    heapinfo = HeapInfo()
+    get_main_arena(heapinfo)
+
     if(heap_info[0]==-1):
         return "heap       uninitialized"
     else:
         heapstring = "\n".join([
-            "heap       %s-%s" % (hex(heap_info[0]), hex(heap_info[1]))
-          
+            "heap       %s-%s" % (hex(heap_info[0]), hex(heap_info[1])),
+            "main_arena %s-s" % (hex(heap_info[0])) 
             ])
         return heapstring
+
+def get_main_arena(heapinfo):
+    allocator = pwndbg.heap.current
+    heapinfo.main_arena[0] = allocator.get_arena()["top"]
