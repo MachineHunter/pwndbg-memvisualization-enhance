8cdaef5ab4ab11fd16c0db88a0c419f08e1466ec
natari57 <natatsuka@nsl.cs.waseda.ac.jp>
2021-10-07 15:25:48 +0900

    GUI files

--- pwndbg/memviewGUI/initGUI.py
+++ pwndbg/memviewGUI/initGUI.py
@@ -9,7 +9,7 @@ logging.getLogger("kivy").disabled = True
 def init():
     global app    
     app = pwndbg.memviewGUI.memGUI.MemoryApp()    
-    th_app = threading.Thread(target=pwndbg.memviewGUI.memGUI.app_run, args=(app,))    
+    th_app = threading.Thread(target=pwndbg.memviewGUI.memGUI.app_run, args=(app,), daemon=True)
     th_app.start()
 
 def get_instance():
